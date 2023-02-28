bc758a34b93425d3a5be1bb51dec27f1382c2417
Bennett Gunson <bengunson@gmail.com>
2023-02-28 13:05:54 -0700

    add /version & /peers endpoint

--- proxy/src/routes.js
+++ proxy/src/routes.js
@@ -8,42 +8,63 @@ const router = express.Router();
 const { getDefaultAd } = require('./defaults');
 const { isValidType } = require('./validators');
 
-/**
- * GET /ad
- */
-router.use('/ad', (req, res) => {
-
-    // get the peers
-    const peers = req.app.get('peers');
-    // choose a peer
-    const socket = peers.choosePeer();
-
-    if (!socket) {
-        // no servers online
-        console.log("ERROR: No peers online! Serving default ad!");
-        res.sendFile(getDefaultAd());
-        return;
+router.use('/version', 
+    /**
+     * @param {express.Request} req - GET /version 
+     * @param {express.Response} res  Response: version from package.json
+     */
+    (req, res) => {
+        const { version } = require('../package.json');
+        res.send(version);
     }
-    
-    socket.emit("get-ad");  // tell them we want an ad
+);
 
-    // wait for the stream
-    socket.once("give-ad", (fName, stream) => {
+router.use('/peers', 
+    /**
+     * @param {express.Request} req - GET /peers
+     * @param {express.Response} res - Response: give the peer list of ids
+     */
+    (req, res) => {
+        const peerList = req.app.get('peers').getPeerList();
+        res.send(peerList);
+    }
+);
 
-        // test file name from peer
-        const fType = isValidType(fName);
-        
-        // if fType is undefined (not valid image or asset) bad, else forward to client 
-        if (!fType) {
-            // fallabck here or re-request
-            res.sendFile(getDefaultAd());  
+router.use('/ad', 
+    /**
+     * @param {express.Request} req - GET /ad
+     * @param {express.Response} res - Response: a file containing advertisment
+     */
+    (req, res) => {
+        // get the peers
+        const peers = req.app.get('peers');
+        // choose a peer
+        const socket = peers.choosePeer();
+
+        if (!socket) {
+            // no servers online
+            console.log("ERROR: No peers online! Serving default ad!");
+            res.sendFile(getDefaultAd());
         } else {
-            res.contentType(fName);
-            res.send(stream);
-        }    
+            socket.emit("get-ad");  // tell them we want an ad
 
-    });
+            // wait for the stream
+            socket.once("give-ad", (fName, stream) => {
 
-});
+                // test file name from peer
+                const fType = isValidType(fName);
+                
+                // if fType is undefined (not valid image or asset) bad, else forward to client 
+                if (!fType) {
+                    // fallabck here or re-request
+                    res.sendFile(getDefaultAd());  
+                } else {
+                    res.contentType(fName);
+                    res.send(stream);
+                }    
+            }); 
+        }
+    }
+);
 
 module.exports = router;
