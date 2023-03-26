f23480424c1fdc51dbcb6c5a608b76a8514648e1
Kirill Larshin <klarshin@Kirills-MacBook-Pro.local>
2023-03-25 18:28:40 -0600

    fixed GUI spacing

--- peer/frontend/src/components/Home.jsx
+++ peer/frontend/src/components/Home.jsx
@@ -91,8 +91,8 @@ class Home extends Component {
       <main className="container">
         <h1 className="text-white text-uppercase text-center my-4"></h1>
         <div className="row">
-          <div className="col-md-6 col-sm-10 mx-auto p-0">
-            <div className="card p-3">
+          <div className="col-xl-10 mx-auto p-0">
+            <div className="card p-4 mx-auto w-100">
               <form action="/ad" method="post" encType="multipart/form-data">
                 <div className="mb-4">
 
